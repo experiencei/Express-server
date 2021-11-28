891116d5fdee02467aa34603c9aba7333b049032
experiencei <ayelojahighbee01@gmail.com>
2021-11-28 10:37:50 +0100

    working with express server and body parser

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000;
+
+
+const friends = [
+    { id : 1 , name : 'Sir Isacc Newton'},
+    { id : 2 , name : 'Sir Albert Eistein'},
+    { id : 3 , name : 'Nikola Tesla'}
+]
+
+app.use((req, res, next) => {
+    const start = Date.now()
+    next();
+    const delta = Date.now() - start
+    console.log(`${req.url} ${req.method} ${delta}ms`);
+})
+
+app.get("/friends" , (req , res) => {
+    res.status(200).json(friends)
+});
+
+app.get("/friends/:friendsId" , (req , res) => {
+    const quer = Number(req.params.friendsId);
+    const friend = friends[quer];
+    console.log(friend);
+    if(friend) {
+        res.json(friend)
+    }
+    else {
+        res.status(404).json({
+            error : 'Friend is not found!'
+        })
+    }
+})
+app.listen(PORT ,() => {
+    console.log(`app started on port ${PORT}`);
+})
